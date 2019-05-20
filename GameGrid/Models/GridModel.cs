21b0505d001a1ff3ad4d5eff7c76b505bb132308
Sean Cahill <seancahill25@gmail.com>
2019-05-19 22:48:17 -0400

    first commit

---
+++ GameGrid/Models/GridModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameGrid.Models
+{
+    public class GridModel
+    {
+        public bool[,] Gameboard = new bool[3, 3];
+    }
+}
